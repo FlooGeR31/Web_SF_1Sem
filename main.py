e14a51aace15af67a7455f342a7a333e28f5dab3
Denis S <denis13th@gmail.com>
2023-12-12 10:10:07 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests
+from PIL import Image
+from tensorflow.keras.applications import EfficientNetB0
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.efficientnet import preprocess_input
+from tensorflow.keras.applications.efficientnet import decode_predictions
+import numpy as np
+
+# def recognition(img_path):
+
+# Загружаем предварительно обученную модель EfficientNetB0
+model = EfficientNetB0(weights='imagenet')
+
+def recognition_img(url):
+    # Указываем путь к файлу с изображением, который будем использовать для сохранения файла и его использования
+    img_path = 'image.jpg'
+    # Сохраняем JPG из полученного в запросе URL
+    img = Image.open(requests.get(url, stream = True).raw)
+    img.save(img_path)
+
+    # Загружаем изображение в память. EfficientNetB0 рассчитана на изображения размером 224х224
+    img = image.load_img(img_path, target_size=(224, 224))
+    # Выполняем предварительную обработку изображения
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    # Запускаем распознавание
+    preds = model.predict(x)
+    # СО=оставляем строку с ТОП-3 классами с самой большой вероятностью
+    classes = decode_predictions(preds, top=3)[0]
+    res = 'Results:'
+    for cl in classes:
+        res = res + ' ' + cl[1] + ' ' + str(round(cl[2], 2)) + ';'
+    # Возвращаем строку
+    return res[:-1]
