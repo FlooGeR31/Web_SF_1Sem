0f65af8d80b0c68bd4d53eb651d9aaa623fb0042
Denis S <denis13th@gmail.com>
2023-12-15 19:31:33 +0300

    Create new features, tests and adding some details in readme

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fastapi import FastAPI
+import json
+from pathlib import Path
 from transformers import pipeline
 import requests
 from PIL import Image
@@ -6,9 +9,21 @@ from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.efficientnet import preprocess_input
 from tensorflow.keras.applications.efficientnet import decode_predictions
 import numpy as np
-from fastapi import FastAPI
 
-# def recognition(img_path):
+DATAFILE_CONTENT = {"url1" : ["streetcar 0.74", "passenger_car 0.23", "electric_locomotive 0.02"]}
+DATAFILE_PATH = Path("./base.json")
+
+def datafile_load():
+    global DATAFILE_CONTENT
+    global DATAFILE_PATH
+    if DATAFILE_PATH.is_file():
+        with open(DATAFILE_PATH, 'r') as f:
+            DATAFILE_CONTENT = json.load(f)
+    else:
+        with open(DATAFILE_PATH, "w") as f:
+            f.write(json.dumps(DATAFILE_CONTENT, sort_keys=True, indent=4))
+        with open(DATAFILE_PATH, 'r') as f:
+            DATAFILE_CONTENT = json.load(f)
 
 # Загружаем предварительно обученную модель EfficientNetB0
 model = EfficientNetB0(weights='imagenet')
@@ -36,18 +51,63 @@ def recognition_img(url):
     # Возвращаем строку
     return res[:-1]
 
-#Из библиотеки fastAPI воспользуемся методом для анализа настроений
+def result_to_array(str):
+    x = str.split("; ")
+    return x
+
+def set_new_record(url):
+    global DATAFILE_CONTENT
+    global DATAFILE_PATH
+
+    datafile_load()
+
+    with open(DATAFILE_PATH, 'r+') as f:
+        DATAFILE_CONTENT = json.load(f)
+        DATAFILE_CONTENT[url] = result_to_array(recognition_img(url)[9:])
+        f.seek(0)
+        f.write(json.dumps(DATAFILE_CONTENT, sort_keys=True, indent=4))
+        f.truncate()
+    print("Data:")
+    print(DATAFILE_CONTENT)
+    # Запись нового Json в файл
+    return DATAFILE_CONTENT
+
+def print_answer(url):
+    global DATAFILE_CONTENT
+    datafile_load()
+    if url in DATAFILE_CONTENT:
+        return "This image was recognize. Result - " + DATAFILE_CONTENT[url][0] + "; " + DATAFILE_CONTENT[url][1] + "; " + DATAFILE_CONTENT[url][2] + "."
+    else:
+        if check_is_image(url):
+            set_new_record(url)
+            return "Image recognizing... Result - " + DATAFILE_CONTENT[url][0] + "; " + DATAFILE_CONTENT[url][1] + "; " + DATAFILE_CONTENT[url][2] + "."
+        else:
+            return "This is not image."
+
+def check_is_image(url):
+    try:
+        Image.open(requests.get(url, stream=True).raw)
+        return True
+    except:
+        return False
+
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
 
-# Методы
 # Сообщение-заглушка для метода GET с инструкцией для метода POST
 @app.get("/")
-def getRoot():
+def get_root():
     return {"message": "Use POST + url to JPG image for recognition."}
+
+# Метод GET для получения базы распознаных изображений
+@app.get("/base/")
+def get_base():
+    datafile_load()
+    return DATAFILE_CONTENT
 
 # Метод POST для корня
 @app.post("/")
-def postRoot(url: str):
+def post_root(url: str):
     # Запускаем функцию recognition_img передавая URL JPG картинки из запроса. Возвращаем строку с ТОП-3 классами, которые определила модель
-    return recognition_img(url)
+    return print_answer(url)
+    # return recognition_img(url)
--- test_main.py
+++ test_main.py
@@ -1,11 +1,16 @@
 from fastapi.testclient import TestClient
 from main import app
+import json
+from pathlib import Path
 
 client = TestClient(app)
 
 train = "https://static.independent.co.uk/2022/03/31/00/d0b722b78358bd1ddf031c75bdf52512Y29udGVudHNlYXJjaGFwaSwxNjQ4NzI3NDU3-2.42274411.jpg"
 shuttle = "https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/MSNBC/Components/Photo/_new/121002_AtlantisPhoto-1045a_files.jpg"
 submarine = "https://anna-news.info/wp-content/uploads/2020/06/20/1400/nuclear-submarine-traveling-underwater-e1592642849265.jpg"
+# tram "Results: streetcar 0.74; passenger_car 0.23; electric_locomotive 0.02"
+# shuttle "Results: space_shuttle 0.96; warplane 0.01; airliner 0.0"
+# submarine "Results: submarine 0.89; missile 0.01; projectile 0.01"
 
 def test_get():
     response = client.get("/")
@@ -18,6 +23,7 @@ def test_post_submarine():
     )
 
     json_data = response.json()
+    data_cleaning(submarine)
     assert response.status_code == 200
     assert "submarine 0.86" in str(json_data)
 
@@ -27,6 +33,7 @@ def test_post_train():
     )
 
     json_data = response.json()
+    data_cleaning(train)
     assert response.status_code == 200
     assert "electric_locomotive 0.22" in str(json_data)
 
@@ -36,5 +43,34 @@ def test_post_shuttle():
     )
 
     json_data = response.json()
+    data_cleaning(shuttle)
     assert response.status_code == 200
     assert "space_shuttle 0.85" in str(json_data)
+
+def test_get_all_data():
+    response = client.get("/base/")
+    assert response.status_code == 200
+    assert Path("./base.json").is_file()
+    with open(Path("./base.json"), 'r') as openfile:
+        datafile_content = json.load(openfile)
+    print(response.json())
+    print(datafile_content)
+    assert response.json() == datafile_content
+
+def test_not_image_url():
+    response = client.post("/",
+        params={"url": "not_a_url"}
+    )
+    assert response.status_code == 200
+    assert response.json() == "This is not image."
+
+def data_cleaning(url):
+    datafile_path = Path("./base.json")
+    if datafile_path.is_file():
+        with open(datafile_path, 'r+') as f:
+            datafile_content = json.load(f)
+            if url in datafile_content:
+                del datafile_content[url]
+            f.seek(0)
+            f.write(json.dumps(datafile_content, sort_keys=True, indent=4))
+            f.truncate()
